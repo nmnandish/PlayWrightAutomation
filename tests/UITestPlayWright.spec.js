678aa6254382024326c59bc3a4ab83c0c3098189
nmnandish <nandish298@gmail.com>
2026-04-13 21:52:20 +0530

    validate login button

--- tests/UITestPlayWright.spec.js
+++ tests/UITestPlayWright.spec.js
@@ -1,19 +1,19 @@
 const {test, expect} = require('@playwright/test')
 
-// test('MyNewTest', async ({ page }) => {
-//     await page.goto('https://www.amazon.com/');
-//    // await page.getByRole('link', { name: 'Get started' }).click();
-//     // await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright');
-//     // console.log(await page.title());
-//     await page.locator('input#twotabsearchtextbox').fill('s26 ultra');
-//     await page.locator('#nav-search-submit-button').click();
-// });
+test('MyNewTest', async ({ page }) => {
+    await page.goto('https://www.amazon.com/');
+   // await page.getByRole('link', { name: 'Get started' }).click();
+    // await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright');
+    // console.log(await page.title());
+    await page.locator('input#twotabsearchtextbox').fill('s26 ultra');
+    await page.locator('#nav-search-submit-button').click();
+});
 
-test('ENdtoENdTest', async({page})=>
+test.only('ENdtoENdTest', async({page})=>
 {
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
     await page.locator("input#userEmail").fill('Kohli18@gmail.com');
     await page.locator("input#userPassword").fill('Welcome@123');
     await page.locator("[value='Login']").click();
-    expect(await page.locator("[value='Login']").toBeVisible()).isFalsy();
+    await expect(page.locator("[value='Login']").isHidden()).toBeTruthy();
 }); 

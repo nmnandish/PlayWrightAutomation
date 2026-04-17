289a55f0fc30e938b252a4395db4263bf09d9d97
nmnandish <nandish298@gmail.com>
2026-04-17 18:26:42 +0530

    API AUth

---
+++ Fixtures/APIAuth.fixture.js
@@ -0,0 +1,14 @@
+const payload = { userEmail: "Kohli18@gmail.com", userPassword: "Welcome@123" };
+
+export const APIFix = {
+    LoginViaAPI: async ({ request}, use ) => {
+        const loginResponse = await request.post("https://rahulshettyacademy.com/api/ecom/auth/login", {
+            data: payload
+        });
+        const loginData = await loginResponse.json();
+        // loginResponse.ok() ? console.log("Login successful") : console.log("Login failed");
+        await use(loginData);
+    }
+};
+
+

24f3d864503f942b880153f12f9ec26bfb56b040
nmnandish <nandish298@gmail.com>
2026-04-19 19:07:57 +0530

    Fix for failure

--- tests/UITestPlayWright.spec.js
+++ tests/UITestPlayWright.spec.js
@@ -4,10 +4,10 @@ import { test, expect } from '../Fixtures/App.Fixtures.js';
 test('Login to UAT and validate the home page', async ({ HomePage, LoginPage }) => {
     await LoginPage.LoginToApp("Kohli18@gmail.com", "Welcome@123");
     const lab = await HomePage.DoValidateTheHome();
-    await expect(lab.label).toEqual("Join Rahul Shetty for a QA Career Meetup in CHENNAI — Book Your Spot");
-    await expect(lab.label).toContain("Join Rahul Shetty for a QA Career Meetup in CHENNAI — Book Your Spot");
-    await expect(lab.label).toBe("Join Rahul Shetty for a QA Career Meetup in CHENNAI — Book Your Spot");
-    await expect(lab.label).toContain("Book Your Spot");
+    await expect(lab.label).toEqual("Get Shortlisted by Recruiters - Take QA Skill Assessments on TechSmartHire");
+    await expect(lab.label).toContain("- Take QA Skill Assessments on TechSmartHire");
+    await expect(lab.label).toBe("Get Shortlisted by Recruiters - Take QA Skill Assessments on TechSmartHire");
+    await expect(lab.label).toContain("TechSmartHire");
     console.log(lab.signOut);
     await expect(lab.signOut).toBeTruthy();
 });
@@ -15,7 +15,7 @@ test('Login to UAT and validate the home page', async ({ HomePage, LoginPage }) 
 test('Test Adding Item to Cart', async ({ HomePage, LoginPage }) => {
     await LoginPage.LoginToApp("Kohli18@gmail.com", "Welcome@123");
     const lab = await HomePage.DoValidateTheHome();
-    await expect(lab.label).toContain("Book Your Spot");
+    await expect(lab.label).toContain("TechSmartHire");
     await HomePage.AddItemToCart("ZARA COAT 3");
     console.log("Item Added to Cart");
 });
@@ -25,7 +25,7 @@ test('Login Test Via API', async ({ LoginViaAPI }) => {
     console.log(LoginViaAPI.message);
 });
 
-//
+// This test is to validate the API login and response via APIUtil class in Fixture.
 test('Login via API from APIUtils', async ({ page, APILOGIN, HomePage }) => {
     await console.log(APILOGIN.message);
 
@@ -33,8 +33,8 @@ test('Login via API from APIUtils', async ({ page, APILOGIN, HomePage }) => {
     await page.addInitScript(value => {
         window.localStorage.setItem('token', value);
     }, APILOGIN.token);
-    // Now navigate to the page, and it should recognize the user as logged in.
 
+    // Now navigate to the page, and it should recognize the user as logged in.
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
 
     //Locating the elements like below is not efficient, we can create the member functions or variables in the HomePage POM.
